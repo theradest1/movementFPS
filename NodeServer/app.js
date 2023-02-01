a65ca5f9a5f3473ea0bbc0b59f8b7e0871c6f319
TheRad1 <18326@verona.k12.wi.us>
2023-02-01 08:46:12 -0600

    Update app.js

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,6 +1,9 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
+playerPorts = {};
+playerAddresses = {};
+
 server.on('error', (err) => {
 	console.log(`server error:\n${err.stack}`);
 	server.close();
