d18de84f2ea128d8437d95b616fa0ffec24655c9
TheRad1 <18326@verona.k12.wi.us>
2023-02-06 12:01:34 -0600

    still working on wall jumping/sliding
    just need to figure out how to get rid of all velocity going towards the wall

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,7 @@ public class MainMenu : MonoBehaviour
     //public static int clientPort = 5000;
     public static string address = "localhost";//"192.168.0.50";
     public static string username = "joe";
+    public int usernameLengthLimit;
 
     public TMP_InputField portInput;
     //public TMP_InputField clientPortInput;
@@ -23,6 +24,16 @@ public class MainMenu : MonoBehaviour
 
     // Update is called once per frame
     public void updateInfo(){
+        string allowedChars = "1234567890abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUFWYXZ_";
+		int strLen = usernameInput.text.Length;
+		if(strLen >= 1){
+			if(!allowedChars.Contains(usernameInput.text[strLen - 1])){
+				usernameInput.text = usernameInput.text.Substring(0, strLen - 1);
+			}
+		}
+		if(usernameInput.text.Length > usernameLengthLimit){
+			usernameInput.text = usernameInput.text.Substring(0, usernameLengthLimit);
+		}
         port = int.Parse(portInput.text);
         //clientPort = int.Parse(clientPortInput.text);
         address = addressInput.text;
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -23,12 +23,21 @@ public class movement : MonoBehaviour
     public TextMeshProUGUI velocityText;
     public bool attatched;
     public bool ableToAttatch = true;
+    public bool ableToJumpOff = true;
+    public float ableToJumpOffTimer = 0f;
+    public float timeBeforeAbleToJumpOff = .2f;
     public float ableToAttatchTimer = 0f;
     public float timeBeforeAbleToAttatch = 2f;
     public float wallJumpVerticalPower;
     public float wallJumpPower;
 
     public GameObject cam;
+    public look lookScript;
+    public float lookSpeedHorizontal;
+    public float speedReductionPerDegree;
+    public float minDegreesToEffectSpeed;
+    public float inAirStrafingSpeed;
+    public float maxSpeed;
 
 
     void OnDrawGizmos(){
@@ -37,12 +46,17 @@ public class movement : MonoBehaviour
         Gizmos.DrawSphere(transform.position + new Vector3(0f, -.5f, 0f), .5f);
     }
 
+    void Start(){
+        lookSpeedHorizontal = lookScript.lookSpeedHorizontal;
+    }
+
     // Update is called once per frame
     void Update()
     {
         //isGrounded = Physics.CheckSphere(groundCheck.position, .5f, groundMask);
         //bool colliding = Physics.CheckCapsule(transform.position + new Vector3(0f, .5f, 0f), transform.position + new Vector3(0f, -.5f, 0f), .5f, groundMask);
         //Debug.Log("Colliding: " + colliding);
+
         Vector2 moveDirection = controlsManagerScript.moveDirection;
 
         if(isGrounded && controlsManagerScript.jumping){
@@ -50,8 +64,7 @@ public class movement : MonoBehaviour
             //isGrounded = false;
         }
 
-        if(attatched && controlsManagerScript.jumping){
-            Debug.Log("jumped off");
+        if(attatched && controlsManagerScript.jumping && ableToJumpOff){
             attatched = false;
             velocity.y = wallJumpVerticalPower;
             velocity += cam.transform.forward * wallJumpPower;
@@ -62,16 +75,22 @@ public class movement : MonoBehaviour
         if(ableToAttatchTimer <= 0f && !attatched){
             ableToAttatch = true;
         }
+        ableToJumpOffTimer -= Time.deltaTime;
+        if(ableToJumpOffTimer <= 0f && attatched){
+            ableToJumpOff = true;
+        }
 
         if(isGrounded){
             velocity += speed * transform.right * moveDirection.x * Time.deltaTime;
             velocity += speed * transform.forward * moveDirection.y * Time.deltaTime;
-            //velocity.y = Mathf.Clamp(velocity.y, 0f, 99999999999f);
-            //velocity.x *= stopSpeedGround;
-            //velocity.z *= stopSpeedGround;
         }
         else if(attatched == false){
             velocity.y += gravity * Time.deltaTime;
+            velocity = Quaternion.AngleAxis(controlsManagerScript.mouseDelta.x * lookSpeedHorizontal, Vector3.up) * velocity;
+            if(controlsManagerScript.mouseDelta.x * lookSpeedHorizontal * Time.deltaTime > minDegreesToEffectSpeed){
+                velocity *= Mathf.Pow(speedReductionPerDegree, controlsManagerScript.mouseDelta.x * lookSpeedHorizontal);
+                Debug.Log("reduced speed");
+            }
             
         }
         else{
@@ -79,6 +98,13 @@ public class movement : MonoBehaviour
         }
 
         //Debug.Log(velocity);
+        float yVel = velocity.y;
+        velocity.y = 0f;
+        Debug.Log("-------------------");
+        Debug.Log(velocity.magnitude);
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        Debug.Log(velocity.magnitude);
+        velocity.y = yVel;
         applyVelocity(velocity);
     }
 
@@ -111,16 +137,20 @@ public class movement : MonoBehaviour
         bool possibleAttatched = false; //just so you don't randomly get flung off because update is called in between apply velocities (not sure how)
         if(Physics.CheckCapsule(transform.position + new Vector3(0f, .5f, 0f), transform.position + new Vector3(0f, -.5f, 0f), .5f, groundMask)){
             transform.position -= new Vector3(0f, 0f, velocity.z);
+            velocity.z = 0f;
             possibleAttatched = isGrounded == false;
         }
         
         transform.position += new Vector3(velocity.x, 0f, 0f);
         if(Physics.CheckCapsule(transform.position + new Vector3(0f, .5f, 0f), transform.position + new Vector3(0f, -.5f, 0f), .5f, groundMask)){
             transform.position -= new Vector3(velocity.x, 0f, 0f);
+            velocity.x = 0f;
             possibleAttatched = isGrounded == false;
         }
         if(possibleAttatched && ableToAttatch && !attatched){
             attatched = true;
+            ableToJumpOff = false;
+            ableToJumpOffTimer = timeBeforeAbleToJumpOff;
             ableToAttatch = false;
         }
         if(!possibleAttatched){
