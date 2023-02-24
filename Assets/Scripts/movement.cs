0e36cccd4b3a78cf3dc4abcb7ff1008d97aa40e2
TheRad1 <18326@verona.k12.wi.us>
2023-02-23 19:57:02 -0600

    added sliding (still changing it a bit)

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -13,7 +13,9 @@ public class movement : MonoBehaviour
     TextMeshProUGUI velocityText;
 
     Vector3 velocity = new Vector3(0f, 0f, 0f);
-    bool isGrounded = true;
+    bool isGrounded = false;
+    bool isSliding = false;
+    bool ableToJump = false;
 
     public LayerMask groundMask;
     public float gravity;
@@ -21,12 +23,18 @@ public class movement : MonoBehaviour
     public float speed;
     public float jumpPower;
     public float stopSpeedGround;
+    public float stopSpeedSliding;
     public float stopSpeedAir;
     public float maxSpeed;
     public float sprintMultiplier;
 
     public float footstepInterval;
     public Vector2 lastFootstepPos;
+
+    public Vector3 camPosSliding;
+    public float camPosSpeed;
+    public Vector3 camPosNotSliding;
+    
 
     void OnDrawGizmos(){
         Gizmos.color = Color.yellow;
@@ -46,27 +54,58 @@ public class movement : MonoBehaviour
     void Update()
     {
         Vector2 moveDirection = controlsManager.moveDirection;
+        if(!controlsManager.jumping){
+            ableToJump = true;
+        }
+        
+        Vector3 goToPos;
+        if(isSliding){
+            goToPos = camPosSliding;
+        }
+        else{
+            goToPos = camPosNotSliding;
+        }
+        cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, goToPos, camPosSpeed * Time.deltaTime);
 
-        if(Vector2.Distance(lastFootstepPos, new Vector2(transform.position.x, transform.position.z)) >= footstepInterval && isGrounded){
+        if(Vector2.Distance(lastFootstepPos, new Vector2(transform.position.x, transform.position.z)) >= footstepInterval && isGrounded && !isSliding){
             lastFootstepPos = new Vector2(transform.position.x, transform.position.z);
             serverEvents.sendEvent("universalEvent", "sound", Random.Range(2, 6) + "~" + transform.position + "~1~1");
         }
 
-        if(isGrounded && controlsManager.jumping){
+        if(isGrounded && controlsManager.jumping && !isSliding && ableToJump){
             velocity.y = jumpPower;
+            ableToJump = false;
+            isGrounded = false;
         }
-
-        if(isGrounded && !controlsManager.sprinting){
-            velocity += speed * transform.right * moveDirection.x * Time.deltaTime;
-            velocity += speed * transform.forward * moveDirection.y * Time.deltaTime;
-        }
-        else if(isGrounded && controlsManager.sprinting){
-            velocity += speed * transform.right * moveDirection.x * sprintMultiplier * Time.deltaTime;
-            velocity += speed * transform.forward * moveDirection.y * sprintMultiplier * Time.deltaTime;
+        //Debug.Log(velocity);
+        if(isGrounded && controlsManager.crouching){
+            if(!isSliding){
+                float velocityMag = velocity.magnitude;
+                velocity = cam.transform.forward * velocityMag;
+            }
+            isSliding = true;
         }
         else{
+            isSliding = false;
+        }
+
+        if(isGrounded && !isSliding){
+            velocity.y  = -1f;
+        }
+
+        if(isGrounded && !isSliding){
+            if(controlsManager.sprinting){
+                velocity += speed * transform.right * moveDirection.x * sprintMultiplier * Time.deltaTime;
+                velocity += speed * transform.forward * moveDirection.y * sprintMultiplier * Time.deltaTime;
+            }
+            else{
+                velocity += speed * transform.right * moveDirection.x * Time.deltaTime;
+                velocity += speed * transform.forward * moveDirection.y * Time.deltaTime;
+            }
+        }
+        else if(!isSliding){
             velocity.y += gravity * Time.deltaTime;
-            velocity = Quaternion.AngleAxis(controlsManager.mouseDelta.x * look.LookSpeedHorizontal, Vector3.up) * velocity;
+            //velocity = Quaternion.AngleAxis(controlsManager.mouseDelta.x * look.LookSpeedHorizontal, Vector3.up) * velocity;
             if(transform.position.y < -100){
                 transform.position = new Vector3(0f, 20f, 0f);
             }
@@ -76,23 +115,27 @@ public class movement : MonoBehaviour
     }
 
     void FixedUpdate() {
-        if(!isGrounded){
-            velocity.x *= stopSpeedAir;
-            velocity.z *= stopSpeedAir;
+        if(isGrounded){
+            if(isSliding){
+                velocity.x *= stopSpeedSliding;
+                velocity.z *= stopSpeedSliding;
+            }
+            else{
+                velocity.x *= stopSpeedGround;
+                velocity.z *= stopSpeedGround;
+            }
         }
         else{
-            velocity.x *= stopSpeedGround;
-            velocity.z *= stopSpeedGround;
+            velocity.x *= stopSpeedAir;
+            velocity.z *= stopSpeedAir;
         }
     }
 
     void applyVelocity(Vector3 velocity){
-        //transform.position += transform.forward * velocity.z + transform.right * velocity.x + new Vector3(0f, velocity.y, 0f);
-        
         transform.position += new Vector3(0f, velocity.y, 0f) * Time.deltaTime;
         if(Physics.CheckCapsule(transform.position + new Vector3(0f, .5f, 0f), transform.position + new Vector3(0f, -.5f, 0f), .5f, groundMask)){
             transform.position -= new Vector3(0f, velocity.y, 0f) * Time.deltaTime;
-            isGrounded = velocity.y <= 0f;
+            isGrounded = true;//velocity.y <= 0f;
             velocity.y = 0f;
         }
         else{
