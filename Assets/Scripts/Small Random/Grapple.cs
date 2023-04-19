ab9310b3f6e7874684772da6325b1de4726a43d5
TheRad1 <18326@verona.k12.wi.us>
2023-04-19 09:06:12 -0500

    climbing

--- Assets/Scripts/Small Random/Grapple.cs
+++ Assets/Scripts/Small Random/Grapple.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
-    public ConfigurableJoint joint;
-    public GameObject cam;
-    public float maxDist;
-    public LayerMask stopFrom;
     ControlsManager controlsManager;
+    ConfigurableJoint joint;
+    [HideInInspector]
+    public bool grappling = false;
+    [HideInInspector]
+    public float distanceToGrapple;
+
+    [Header("References:")]
+    public GameObject cam;
     public GameObject player;
     public LineRenderer lineRenderer;
+    public Rigidbody rb;
 
     [Header("joint settings:")]
-    public float springForce;
-    public float damper;
-    public float massScale;
+    public LayerMask stopFrom;
+    public float maxDist;
     Vector3 connectPos;
     public float climbSpeed;
-    public bool grappling = false;
-    public float distanceToGrapple;
-    public Rigidbody rb;
 
     private void Start() {
         controlsManager = GameObject.Find("manager").GetComponent<ControlsManager>();
@@ -60,6 +61,8 @@ public class Grapple : MonoBehaviour
     }
 
     private void Update() {
+
+
         if(!grappling && controlsManager.grappling){
             attach();
         }
@@ -68,11 +71,12 @@ public class Grapple : MonoBehaviour
             grappling = false;
         }
         if(grappling){
+            SoftJointLimit softJointLimit = new SoftJointLimit();
+            distanceToGrapple -= climbSpeed * Time.deltaTime;
+            softJointLimit.limit = distanceToGrapple;
+            joint.linearLimit = softJointLimit;
+
             if(Physics.Raycast(cam.transform.position, connectPos - cam.transform.position, out RaycastHit hitInfo, Vector3.Distance(cam.transform.position, connectPos), stopFrom)){
-                SoftJointLimit softJointLimit = new SoftJointLimit();
-                if(Vector3.Distance(connectPos, hitInfo.point) > .1f){
-                    Debug.Log(Vector3.Distance(connectPos, hitInfo.point));
-                }
                 distanceToGrapple -= Vector3.Distance(connectPos, hitInfo.point);
                 softJointLimit.limit = distanceToGrapple;
                 joint.linearLimit = softJointLimit;
