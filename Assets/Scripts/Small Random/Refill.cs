aac3f3ca7f231773b088f1b768fe866513b466b9
TheRad1 <18326@verona.k12.wi.us>
2023-05-20 16:39:58 -0500

    small fix

--- Assets/Scripts/Small Random/Refill.cs
+++ Assets/Scripts/Small Random/Refill.cs
@@ -11,6 +11,8 @@ public class Refill : MonoBehaviour
 
     [Header("Heal:")]
     public float healthOnCollect;
+    [Header("Anim")]
+    public float animTime = .03f;
     private void Start()
     {
         Invoke("clean", timeBeforeDestroy);
@@ -22,20 +24,39 @@ public class Refill : MonoBehaviour
             //Debug.Log("Collected refill of type " + type);
             if(type == "tool"){
                 if(GameObject.Find("Player").GetComponent<Tools>().collectCharge()){
-                    GameObject.Find("manager").GetComponent<RefillManager>().sendCollectEvent(ID);
+                    StartCoroutine(collectAnim());
                 }
             }
             else if(type == "health"){
                 if(GameObject.Find("Player").GetComponent<PlayerManager>().collectHealth(healthOnCollect)){
-                    GameObject.Find("manager").GetComponent<RefillManager>().sendCollectEvent(ID);
+                    StartCoroutine(collectAnim());
                 }
             }
             else if(type == "throwable"){
                 if(GameObject.Find("Player").GetComponent<Throwables>().collectThrowableCharge()){
-                    GameObject.Find("manager").GetComponent<RefillManager>().sendCollectEvent(ID);
+                    StartCoroutine(collectAnim());
                 }
             }
         }
+    }
+
+    public IEnumerator collectAnim(){
+        //setup
+        Destroy(transform.parent.GetComponent<Rigidbody>());
+        Destroy(transform.parent.GetComponent<Collider>());
+        Transform parentTransform = transform.parent.transform;
+        Transform playerTransform = GameObject.Find("Player").transform;
+        Vector3 startPos = transform.position;
+
+        //anim
+        float timer = Time.time;
+        while(Time.time - timer <= animTime){
+            parentTransform.position = Vector3.Lerp(startPos, playerTransform.position, (Time.time - timer)/animTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        //trigger
+        GameObject.Find("manager").GetComponent<RefillManager>().sendCollectEvent(ID);
     }
 
     public void clean(){
