0b83f31d1b7f02b3d1ec849ba6436f87b3cb683b
TheRad1 <18326@verona.k12.wi.us>
2023-05-21 11:03:11 -0500

    made refills collectable from farther away

--- Assets/Scripts/Small Random/Refill.cs
+++ Assets/Scripts/Small Random/Refill.cs
@@ -8,33 +8,47 @@ public class Refill : MonoBehaviour
     public string type;
     public int ID;
     public float timeBeforeDestroy = 30;
+    public float distanceToCollect = 2.5f;
+    bool active = true;
 
     [Header("Heal:")]
     public float healthOnCollect;
     [Header("Anim")]
     public float animTime = .03f;
+
+    //player references
+    Transform player;
+    PlayerManager playerManager;
+    Tools tools;
+    Throwables throwables;
+
     private void Start()
     {
+        player = GameObject.Find("Player").transform;
+        tools = GameObject.Find("Player").GetComponent<Tools>();
+        playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+        throwables = GameObject.Find("Player").GetComponent<Throwables>();
         Invoke("clean", timeBeforeDestroy);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.name == "Player"){
-            //Debug.Log("Collected refill of type " + type);
-            if(type == "tool"){
-                if(GameObject.Find("Player").GetComponent<Tools>().collectCharge()){
-                    StartCoroutine(collectAnim());
+    private void Update() {
+        if(active){
+            if(Vector3.Distance(transform.position, player.position) <= distanceToCollect){
+                Debug.Log("close enough");
+                if(type == "tool"){
+                    if(tools.collectCharge()){
+                        StartCoroutine(collectAnim());
+                    }
                 }
-            }
-            else if(type == "health"){
-                if(GameObject.Find("Player").GetComponent<PlayerManager>().collectHealth(healthOnCollect)){
-                    StartCoroutine(collectAnim());
+                else if(type == "health"){
+                    if(playerManager.collectHealth(healthOnCollect)){
+                        StartCoroutine(collectAnim());
+                    }
                 }
-            }
-            else if(type == "throwable"){
-                if(GameObject.Find("Player").GetComponent<Throwables>().collectThrowableCharge()){
-                    StartCoroutine(collectAnim());
+                else if(type == "throwable"){
+                    if(throwables.collectThrowableCharge()){
+                        StartCoroutine(collectAnim());
+                    }
                 }
             }
         }
@@ -42,6 +56,8 @@ public class Refill : MonoBehaviour
 
     public IEnumerator collectAnim(){
         //setup
+        Debug.Log("collect");
+        active = false;
         Destroy(transform.parent.GetComponent<Rigidbody>());
         Destroy(transform.parent.GetComponent<Collider>());
         Transform parentTransform = transform.parent.transform;
@@ -51,7 +67,7 @@ public class Refill : MonoBehaviour
         //anim
         float timer = Time.time;
         while(Time.time - timer <= animTime){
-            parentTransform.position = Vector3.Lerp(startPos, playerTransform.position, (Time.time - timer)/animTime);
+            parentTransform.position = Vector3.Lerp(startPos, playerTransform.position + Vector3.up/3, (Time.time - timer)/animTime);
             yield return new WaitForEndOfFrame();
         }
 
